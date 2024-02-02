5a748ec998ef5180e81230f4ad5080404dcea7c5
deelaka99 <123wejith@gmail.com>
2024-02-02 14:41:09 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="text-2xl text-blue-font font-roboto bg-primary-background h-screen w-screen">
+      <div className="w-full h-1/5 bg-green-background"></div>
+      <div className="w-full h-2/5 bg-white"></div>
+      <div className="w-full h-2/5 bg-blue-font"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      'primary-background':'#F5F5F5',
+      'white' :'#FFFFFF',
+      'dark-font': '#0E0E0E',
+      'blue-font':'#0E42FD',
+      'green-background':'#2BD9AF',
+      'red-background':'#FF5E84',      
+    },
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+    },
+    screens:{
+      'mobile':'375px',
+      'tablet':'768px',
+      'laptop':'1200px',
+      'desktop':'1440px',
+    },
+  },
+  plugins: [],
+}
